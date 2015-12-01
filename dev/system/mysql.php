9bc7e761f72c538a2992cfb96e7329f2f1a4140d
bearf <igor.zinovyev@gmail.com>
2015-12-01 17:22:41 +0300

    [CF] Cp1251 support

--- dev/system/mysql.php
+++ dev/system/mysql.php
@@ -4,5 +4,6 @@ global $mysqli_;
 // todo: persistent connections
 $mysqli_ = new mysqli(HostName, UserName, Password, DBName);
 if (mysqli_connect_errno()) { die(mysqli_error());fail(_error_no_mysql_connection_code); }
-$mysqli_->query( 'set names cp1251' );
+//$mysqli_->query( 'set names cp1251' );
+echo 'names done';
 ?>
