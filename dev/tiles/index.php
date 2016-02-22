8abb8668ee53e0159ce4be40a37ec94b04068215
bearf <igor.zinovyev@gmail.com>
2016-02-22 14:59:46 +0300

    Qualification results and new partner logos

--- dev/tiles/index.php
+++ dev/tiles/index.php
@@ -3,11 +3,24 @@
 <a href="#" id="notes" class="anchor"></a>
 <h3>Новости</h3>
 
+<p class="message" style="width:350px;"><strong>Результаты отборочного тура</strong>
+
+<p>Результаты: <a href="https://docs.google.com/spreadsheets/d/13mRwICZqhW6_gzrsmnzmvoa1ke0FRzsgD-HA4nOIzp4/edit?usp=sharing">https://docs.google.com/spreadsheets/d/13mRwICZqhW6_gzrsmnzmvoa1ke0FRzsgD-HA4nOIzp4/edit?usp=sharing</a>
+
+<p>Если ваша команда прошла в основной тур (в результате отбора либо по специальному приглашению), то в срок до 29 февраля включительно вам необходимо:
+    
+    <ul><li>заполнить <a href="https://docs.google.com/forms/d/1LWVDIHPubQ7thRv4E4kD5kXJw7u8SnZc9fnNod1RFoA/viewform">анкету подтверждения участия</a> (одна на команду);
+        <li>заполнить три <a href="https://docs.google.com/forms/d/1aZ956Z_tLWptQJFkuUR01vDuzVShS_cIWW879hu2e-g/viewform">анкеты участника</a> (на каждого из членов команды).</li></ul>
+    
+<p>Если ваша команда была зарегистрирована на отбор, участвовала в нём, но не отображена в таблице, либо не отмечена, как зарегистрированная, пишите на контактный e-mail Наталье Гришиной: <a href="mailto:natalia.grishina@icl-services.com">natalia.grishina@icl-services.com</a>.
+
+<p>Из таблицы результатов исключены команды, не решившие ни одной задачи.
+
 <p class="message" style="width:350px;"><strong>ICL'16 &mdash; 13 марта. Обновлено</strong>
     
 <p>13 марта 2016 года пройдет очередной Турнир ICL. Это соревнование станет уже шестнадцатым по счёту.
     
-<p>Участники Турнира определятся по итогам заочного отборочного тура, который будет проведен на базе одного из этапов <a href="opencup.ru">Открытого кубка по программированию</a> (либо 14, либо 21 февраля). День проведения отбора будет уточнён в ближайшее время. 
+<p>Участники Турнира определятся по итогам заочного отборочного тура, который будет проведен на базе одного из этапов <a href="opencup.ru">Открытого кубка по программированию</a> от 14 февраля.  
     
 <p>Команда-победитель основного тура увезёт домой главный приз &mdash; три ультрабука производства ОАО &laquo;АйСиЭл&mdash;КПО ВС&raquo;. 
     
@@ -20,7 +33,7 @@
     <li>собрать команду из 3 школьников или 3 студентов;</li>
     <li>если ваша команда не имеет <a href="./qualification.php#wildcard">специального приглашения</a>:
         <ul>
-            <li>заполнить <a href="https://docs.google.com/forms/d/1Pj_4BbNjzBjC-DCe5qkIL6cFRBV-ATdZnbllzdUUXIk/viewform" target="_blank">форму регистрации на отборочный тур</a> до наступления дня его проведения. Тур будет проведен либо 14, либо 21 февраля;</li>
+            <li>заполнить <a href="https://docs.google.com/forms/d/1Pj_4BbNjzBjC-DCe5qkIL6cFRBV-ATdZnbllzdUUXIk/viewform" target="_blank">форму регистрации на отборочный тур</a> до наступления дня его проведения (14 февраля);</li>
             <li>пройти <a href="qualification.php">отборочный тур</a>;</li>
         </ul>
     <li>в случае попадания в основной тур (в результате отбора либо по специальному приглашению) в срок до 29 февраля включительно заполнить анкету подтверждения участия (одна на команду) и анкету участника (на каждого из членов команды).</li>
@@ -50,7 +63,7 @@
 <td>Объявление результатов отбора</td></tr-->
 <tr><td>12 марта, суббота</td>
 <td>Открытие и пробный тур</td></tr>
-<tr><td>13 марта, всокресенье</td>
+<tr><td>13 марта, воскресенье</td>
 <td>Основной тур и закрытие</td></tr>
 <!--tr><th colspan="2">14 марта, суббота (время московское)</th></tr>
 <tr>
@@ -135,19 +148,38 @@
 
 <a href="#" id="partners" class="anchor"></a>
 <h3 style="margin-top:16px;">Партнеры</h3>
-<div style="transform:scale(0.60);transform-origin:0 0">
-    <p style="width:400px;"><a href="http://mic.tatarstan.ru/" style="float:left;margin-right:50px;"><img src="./graphic/logo.mis.small.png" alt="Министерство информатизации и связи РТ" width="188" height="188" /></a><a href="http://itpark-kazan.ru" style="float:left;"><img src="./graphic/logo.it.park.png" alt="Казанский IT-парк" width="162" height="226" /></a></p>
-    <p style="clear:both;"><a href="http://www.microsoft.com/rus/"><img src="./graphic/microsoft.jpg" alt="Microsoft" width="466" height="88" style="vertical-align:middle;" /></a></p>
-    <p><a href="http://www.intel.com/#/ru_RU_05"><img src="./graphic/intel.jpg" alt="Intel Channel Partner 2010" width="225" height="150" style="vertical-align:middle;" /><a></p>
-    <p><a href="http://www.kpfu.ru"><img src="./graphic/logo.kfu.jpg" alt="Казанский (Приволжский) федеральный университет" width="400" height="271" /></a></p>
-    <p><a href="http://www.kai.ru"><img src="./graphic/logo.knitu.jpg" alt="Казанский национальный исследовательский технический университет им. А.Н.Туполева" width="400" height="176" /></a></p>
-    <!--p><a href="http://www.kgeu.ru"><img src="./graphic/logo.kgeu.png" alt="Казанский государственный энергетический университет" width="450" height="221" /></a></p-->
-    <!--p><a href="http://www.kp.ru/"><img src="./graphic/logo.kp.png" alt="Комсомольская правда" width="450" height="89" /></a></p-->
-    <p><a href="http://www.rt-online.ru"><img src="./graphic/logo.rt.newspaper.jpg" alt="Газета Республика Татарстан" width="450" height="152" /></a></p>
+<div style="width:600px;">
+    <p style="width:150px;height:150px;padding:25px;float:left;background:url(./graphic/~logo.mis.png) center center no-repeat;"><a style="text-decoration:none;display:block;width:150px;height:150px;" href="http://mic.tatarstan.ru/">&nbsp;</a></p>
+    <p style="width:150px;height:150px;padding:25px;float:left;background:url(./graphic/~logo.kpfu.png) center center no-repeat;"><a style="text-decoration:none;display:block;width:150px;height:150px;" href="http://kpfu.ru/">&nbsp;</a></p>
+    <p style="width:150px;height:150px;padding:25px;float:left;background:url(./graphic/~logo.knitu.png) center center no-repeat;"><a style="text-decoration:none;display:block;width:150px;height:150px;" href="http://kai.ru">&nbsp;</a></p>
+
+    <p style="width:150px;height:150px;padding:25px;float:left;background:url(./graphic/~logo.microsoft.png) center center no-repeat;"><a style="text-decoration:none;display:block;width:150px;height:150px;" href="http://www.microsoft.com/ru-ru">&nbsp;</a></p>
+    <p style="width:150px;height:150px;padding:25px;float:left;background:url(./graphic/~logo.intel.png) center center no-repeat;"><a style="text-decoration:none;display:block;width:150px;height:150px;" href="http://www.intel.ru/content/www/ru/ru/homepage.html">&nbsp;</a></p>
+    <p style="width:150px;height:150px;padding:25px;float:left;background:url(./graphic/~logo.it.park.png) center center no-repeat;"><a style="text-decoration:none;display:block;width:150px;height:150px;" href="http://itpark-kazan.ru">&nbsp;</a></p>
+
+    <p style="width:150px;height:150px;padding:25px;float:left;background:url(./graphic/~logo.itis.png) center center no-repeat;"><a style="text-decoration:none;display:block;width:150px;height:150px;" href="http://kpfu.ru/itis">&nbsp;</a></p>
+    <p style="width:150px;height:150px;padding:25px;float:left;background:url(./graphic/~logo.rt.newspaper.png) center center no-repeat;"><a style="text-decoration:none;display:block;width:150px;height:150px;" href="http://rt-online.ru/">&nbsp;</a></p>
+
+    <p style="width:150px;height:150px;padding:25px;float:left;background:url(./graphic/~logo.all.cio.png) center center no-repeat;"><a style="text-decoration:none;display:block;width:150px;height:150px;" href="http://www.allcio.ru">&nbsp;</a></p>
+    <p style="width:150px;height:150px;padding:25px;float:left;background:url(./graphic/~logo.astera.png) center center no-repeat;"><a style="text-decoration:none;display:block;width:150px;height:150px;" href="http://www.astera.ru">&nbsp;</a></p>
+    <p style="width:150px;height:150px;padding:25px;float:left;background:url(./graphic/~logo.astra.png) center center no-repeat;"><a style="text-decoration:none;display:block;width:150px;height:150px;" href="http://www.astra-partners.ru">&nbsp;</a></p>
+    <p style="width:150px;height:150px;padding:25px;float:left;background:url(./graphic/~logo.business.online.png) center center no-repeat;"><a style="text-decoration:none;display:block;width:150px;height:150px;" href="http://www.business-gazeta.ru">&nbsp;</a></p>
+    <p style="width:150px;height:150px;padding:25px;float:left;background:url(./graphic/~logo.cnews.png) center center no-repeat;"><a style="text-decoration:none;display:block;width:150px;height:150px;" href="http://www.cnews.ru">&nbsp;</a></p>
+    <p style="width:150px;height:150px;padding:25px;float:left;background:url(./graphic/~logo.crn.png) center center no-repeat;"><a style="text-decoration:none;display:block;width:150px;height:150px;" href="http://www.crn.ru">&nbsp;</a></p>
+    <p style="width:150px;height:150px;padding:25px;float:left;background:url(./graphic/~logo.fine.street.png) center center no-repeat;"><a style="text-decoration:none;display:block;width:150px;height:150px;" href="http://www.finestreet.ru">&nbsp;</a></p>
+    <p style="width:150px;height:150px;padding:25px;float:left;background:url(./graphic/~logo.global.cio.png) center center no-repeat;"><a style="text-decoration:none;display:block;width:150px;height:150px;" href="http://www.globalcio.ru">&nbsp;</a></p>
+    <p style="width:150px;height:150px;padding:25px;float:left;background:url(./graphic/~logo.hightech.fm.png) center center no-repeat;"><a style="text-decoration:none;display:block;width:150px;height:150px;" href="http://hightech.fm">&nbsp;</a></p>
+    <p style="width:150px;height:150px;padding:25px;float:left;background:url(./graphic/~logo.it.media.png) center center no-repeat;"><a style="text-decoration:none;display:block;width:150px;height:150px;" href="http://www.allcio.ru">&nbsp;</a></p>
+    <p style="width:150px;height:150px;padding:25px;float:left;background:url(./graphic/~logo.it.weekly.png) center center no-repeat;"><a style="text-decoration:none;display:block;width:150px;height:150px;" href="http://www.it-weekly.ru">&nbsp;</a></p>
+    <p style="width:150px;height:150px;padding:25px;float:left;background:url(./graphic/~logo.it.world.png) center center no-repeat;"><a style="text-decoration:none;display:block;width:150px;height:150px;" href="http://www.it-world.ru">&nbsp;</a></p>
+
+    <p style="width:150px;height:150px;padding:25px;float:left;background:url(./graphic/~logo.real.time.png) center center no-repeat;"><a style="text-decoration:none;display:block;width:150px;height:150px;" href="http://realnoevremya.ru">&nbsp;</a></p>
+    <p style="width:150px;height:150px;padding:25px;float:left;background:url(./graphic/~logo.russoft.png) center center no-repeat;"><a style="text-decoration:none;display:block;width:150px;height:150px;" href="http://www.russoft.ru">&nbsp;</a></p>
+    <p style="width:150px;height:150px;padding:25px;float:left;background:url(./graphic/~logo.tadviser.png) center center no-repeat;"><a style="text-decoration:none;display:block;width:150px;height:150px;" href="http://www.tadviser.ru">&nbsp;</a></p>
+    <p style="width:150px;height:150px;padding:25px;float:left;background:url(./graphic/~logo.tatcenter.png) center center no-repeat;"><a style="text-decoration:none;display:block;width:150px;height:150px;" href="http://info.tatcenter.ru">&nbsp;</a></p>
+
+    <!--<p><a href="http://www.rt-online.ru"><img src="./graphic/logo.rt.newspaper.jpg" alt="Газета Республика Татарстан" width="450" height="152" /></a></p>
     <p><a href="http://ku.kpfu.ru"><img src="./graphic/logo.ku.kpfu.png" alt="Газета Казанский университет" width="450" height="97" /></a></p>
-    <p><img src="./graphic/logo.darelfonyn.png" width="450" height="85" /></a></p>
-    <!--p><a href="http://universmotri.ru"><img src="./graphic/logo.universmotri.ru.png" alt="Студенческое телевидение КФУ" width="450" height="172" /></a></p-->
-    <!--p><a href="http://www.moltat.ru"><img src="./graphic/moltat.jpg" alt="Молодежь Татарстана" width="400" height="146" /></a></p>
-    <p><a href="http://www.prokazan.ru"><img src="./graphic/prokazan.jpg" alt="ProKazan.ru" width="314" height="241" /></a></p-->
+    <p><img src="./graphic/logo.darelfonyn.png" width="450" height="85" /></a></p>-->
 </div>
 <?php } ?>
